53f4eaaa16f8a9e6e496ccd354ecd8411168e6d3
Nandhu270 <p.v.nandhakumar27@gmail.com>
2025-07-03 21:57:14 +0530

    SignUp Page Completed

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,18 @@
+import React from "react"
+import { useNavigate } from "react-router-dom"
+
 export default function Home(){
+    const navigate = useNavigate();
     return (
-        <>
-        <h1>home</h1>
-        </>
+        <div style={{padding:'50px',marginLeft:'10px'}}>
+        <h1>Home Page</h1>
+        <button onClick={()=>{
+            navigate('/login')
+        }}>Login</button><br /><br />
+        <button onClick={()=>{
+            navigate('/signup')
+        }}>SignUp</button><br /><br />
+        <button>Change Theme</button><br /><br />
+        </div>
     )
 }
