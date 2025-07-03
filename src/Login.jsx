895c1b18c9b586508302f94af7d62851069bb7d2
PratheesSenthil <pratheessenthilkumar@gmail.com>
2025-07-03 22:40:30 +0530

    login-page completed

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,8 +1,41 @@
+import { useState } from "react"
+import {useNavigate} from 'react-router-dom'
 
 export default function Login(){
+    const [Username,setUsername]=useState("")
+    const [UserPassword,setUserPassword]=useState("")
+    const [Details,setDetails]=useState(JSON.parse(localStorage.getItem('user')))
+    const navigate = useNavigate();
+    console.log(Details);
+    
+    const  handleLogin=(e)=>{
+        e.preventDefault(); 
+        if((Details.email===Username||Details.username===Username)&& Details.password===UserPassword ){
+             navigate('/')
+        }
+        else{
+            alert("Username or password is wrong")
+        }
+        setUsername("")
+        setUserPassword("")
+
+        
+    }
+    
+    
     return(
+        <>
         <div style={{padding:'50px',marginLeft:'10px'}}>
-         <h1>login page</h1>
+            <h1>LOGIN</h1>
+            <form onSubmit={handleLogin}>
+                <label>Username:  </label>
+                <input type="text" placeholder="Username or email" value={Username} onChange={(e)=>{setUsername(e.target.value)}}/><br/><br/>
+                <label>Password:  </label>
+                <input type="password" placeholder="Password" value={UserPassword} onChange={(e)=>{setUserPassword(e.target.value)}}/><br/><br/>
+                <input type="submit" />
+            </form>
         </div>
+         
+        </>
     )
 }
